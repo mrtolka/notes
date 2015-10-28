3cf98d1c9a8b5464c2c68b2d4e896cc18107a51d
mrtolka <mr.tolka@gmail.com>
2015-10-28 17:50:07 +0200

    Day 2

--- public/notes.js
+++ public/notes.js
@@ -8,6 +8,17 @@ module.controller('NotesController',
     function ($scope,$http) {
 
         $scope.notes = [];
+
+        var update = function() {
+            var params = {params: {section:$scope.activeSection}};
+            $http.get("/notes", params).success(function(notes) {
+                $scope.notes = notes;
+                console.log('Notes: ');
+                console.log($scope.notes);
+            });
+        };
+
+
         $scope.add = function() {
             var note = {text: $scope.text};
             $http.post("/notes", note).success(function() {
@@ -16,22 +27,54 @@ module.controller('NotesController',
             });
         };
 
-        $scope.remove = function() {
-          $http.delete("/notes", {params: {id:$scope.noteId}}).success(function() {
+        $scope.remove = function(id) {
+          $http.delete("/notes", {params: {id:id}}).success(function() {
               update();
           });
-
         };
 
-        var update = function() {
-            $http.get("/notes").success(function(notes) {
-                $scope.notes = notes;
-            });
+        $scope.showSection = function(section) {
+            $scope.activeSection = section.title;
+            update();
         };
 
-        update();
+        //*********************** TO FINISH **********************************
+        //$scope.writeSections = function() {
+        //    if ($scope.sections && $scope.sections.length > 0) {
+        //        $http.post("/sections/replace", $scope.sections);
+        //    }
+        //};
+
+        $scope.addSection = function() {
+            if ($scope.newSection.length == 0) return;
+
+            for (var i=0; i<$scope.length; i++) {
+                if ($scope.sections[i].title==$scope.newSection) {return;}
+            }
+
+            var section = {title: $}
+
+        }
+
+        var readSections = function() {
+            $http.get("/sections").success(function(sections) {
+                $scope.sections = sections;
+
+                if ($scope.sections.length > 0) {
+                    $scope.activeSection = $scope.sections[0].title;
+                }
+
+                console.log('Sections: ');
+                console.log($scope.sections);
+                update();
+                });
+        };
+
+        readSections();
 
 
+
+        //update();
 });
 
 
--- server.js
+++ server.js
@@ -7,10 +7,87 @@ var app = express();
 var path = require('path');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'angular_tutorial',
+    resave: true,
+    saveUninitialized: true
+}));
+
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+var ObjectID = require('mongodb').ObjectID;
+
+var db = new Db('tutor',
+    new Server("localhost", 27017, {safe: true},
+        {auto_reconnect: true}, {}));
+
+db.open(function(){
+
+    console.log("mongo db is opened :)");
+
+    db.collection('notes', function(error, notes) {
+        db.notes = notes;
+    });
+
+    db.collection('sections', function(error, sections) {
+        db.sections = sections;
+    });
+});
+
+
+app.get("/notes", function(req, res) {
+   db.notes.find(req.query).toArray(function(err, items) {
+       res.send(items);
+   });
+});
+
+
+app.post("/notes", function(req, res) {
+    var currentdate = new Date();
+    var currentNote = req.body;
+    //currentNote.date = currentdate.getDate() + "." + (currentdate.getMonth()+1) + "." + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes();
+    currentNote.date = currentdate;
+    db.notes.insert(currentNote);
+    res.end();
+});
+
+
+app.delete("/notes", function(req,res) {
+    var id = new ObjectID(req.query.id);
+    db.notes.remove({_id: id}, function(err) {
+        if (err) {
+            console.log(err);
+            res.send("Shit Happens...");
+        } else {
+            res.send("Ok!");
+        }
+    })
+});
+
+
+app.get("/sections", function(req,res) {
+    db.sections.find(req.query).toArray(function(err, items) {
+        res.send(items);
+    });
+});
+
+
+app.post("sections/replace", function(req,resp) {
+    if (req.body.length==0) {resp.end()}
+    db.sections.remove({}, function(err,res) {
+        if (err) console.log(err);
+        db.sections.insert(req.body, function(err, res) {
+            if (err) console.log("err after insert: ", err);
+            resp.end();
+        });
+    });
+});
 
 
 //app.get("/notes", function(req,res) {
@@ -22,45 +99,73 @@ app.use(bodyParser.json());
 //    res.send(notes);
 //});
 
-
-app.use(session({
-    secret: 'angular_tutorial',
-    resave: true,
-    saveUninitialized: true
-}));
+//app.get("/notes", function(req,res) {
+//    res.send(req.session.notes||[]);
+//});
 
 
-app.get("/notes", function(req,res) {
-    res.send(req.session.notes||[]);
-});
+//app.post("/notes", function(req, res) {
+//    if (!req.session.notes) {
+//        req.session.notes = [];
+//        req.session.last_note_id = 0;
+//    }
+//    var note = req.body;
+//    note.id = req.session.last_note_id;
+//    req.session.last_note_id++;
+//    req.session.notes.push(note);
+//    res.end();
+//});
+
+//app.get("/notes", function(req,res) {
+//
+//    if (!req.session.idCounter) {
+//        req.session.idCounter = 0;
+//    }
+//
+//    fs.readFile("notes.json", function(err, result) {
+//        if (result) {
+//            result = ""+result;
+//            result = result.substring(0, result.length - 1);
+//            result = "["+result+"]";
+//            result = result.split("\n").join(",");
+//            res.send(result);
+//        } else {
+//            res.end();
+//        }
+//    });
+//});
+//
+//app.post("/notes", function(req, res) {
+//    if (!req.session.idCounter) {
+//        req.session.idCounter = 0;
+//    }
+//    var note = req.body;
+//    note.id = req.session.idCounter;
+//    req.session.idCounter++;
+//    var noteText = JSON.stringify(note)+"\n";
+//    fs.appendFile("notes.json", noteText, function() {
+//        res.end();
+//    });
+//});
 
 
-app.post("/notes", function(req, res) {
-    if (!req.session.notes) {
-        req.session.notes = [];
-        req.session.last_note_id = 0;
-    }
-    var note = req.body;
-    note.id = req.session.last_note_id;
-    req.session.last_note_id++;
-    req.session.notes.push(note);
-    res.end();
-});
-
-
-app.delete("/notes", function(req,res) {
-    var id = req.query.id;
-    var notes = req.session.notes||[];
-    var updatedNotesList = [];
-    var newid = 0;
-    for (var i=0;i<notes.length;i++) {
-        if (notes[i].id != id) {
-            updatedNotesList.push(notes[i]);
-        }
-    }
-    req.session.notes = updatedNotesList;
-    res.end();
-});
+//app.delete("/notes", function(req,res) {
+//
+//    var id = req.query.id;
+//    var notes = req.session.notes||[];
+//    var updatedNotesList = [];
+//
+//    for (var i=0;i<notes.length;i++) {
+//        if (notes[i].id != id) {
+//            notes[i].id = newid;
+//            newid++;
+//            updatedNotesList.push(notes[i]);
+//        }
+//    }
+//    //req.session.last_note_id = newid;
+//    req.session.notes = updatedNotesList;
+//    res.end();
+//});
 
 
 //-----------------------------------------------------
